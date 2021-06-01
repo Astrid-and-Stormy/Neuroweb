64fee86736194749145f89bb1e73633aa8d260fc
Astrid-and-Stormy <45171447+Astrid-and-Stormy@users.noreply.github.com>
2021-06-01 12:57:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,60 @@
+from flask import Flask, render_template
+from flask import request
 from math import e
-num_of_tops = int(input('Введите количество вершин: '))
-func = int(input('Введите тип функции (1 - линейная, 2 - сигмовидная, 3 - гиперболический тангенс) '))
-inputs = {}  # Список входных значений
-prototypes = {}  #Двумерный Список прообразов
-weights = {}  #Cловарь списков весов дуг
-outputs = {}
-neurons = [] #Веришны, не являющиеся входными
+app = Flask(__name__)
 
-for i in range(num_of_tops):
-    prototypes[i] = list(map(int, input('Введите проообразы вершины ' + str(i) + ' через пробел: ').split()))
-    if len(prototypes[i]) == 0:
-        inputs[i] = float(input('Введите входной сигнал веришны ' + str(i) + ': '))
-    else:
-        neurons.append(i)
-        weights[i] = list(map(float, input('Введите веса дуг, соединяющих вершину ' + str(i) + ' с веришнами ' + ' '.join(map(str, prototypes[i])) + ': ').split()))
 
-def f(a, func):
-    if func == 1:
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route("/learning", methods=['POST', 'GET'])
+def learning():
+    return render_template('Learning.html', data=request.form, Num_of_ribs=int(request.form['Num_of_ribs']),
+                           Num_of_inputs=int(request.form['Num_of_inputs']))
+
+
+@app.route("/answer", methods=['POST', 'GET'])
+def answer():
+    prototypes, inputs, outputs, MSE = counter(request.form)
+    Num_of_tops = int(request.form['Num_of_tops'])
+    return render_template('Answer.html', ans=request.form, prototypes=prototypes,
+                           inputs=inputs, outputs=outputs, MSE=MSE, Num_of_tops=Num_of_tops)
+
+
+def counter(data):
+    outputs = {}
+    inputs = {}
+    for i in range(int(data['Num_of_inputs'])):
+        inputs[data[f'{i}_index']] = float(data[f'{i}_input'])
+        outputs[data[f'{i}_index']] = float(data[f'{i}_input'])
+    prototypes = {}
+    for i in range(int(data['Num_of_ribs'])):
+        if data[f'{i}_in'] in prototypes.keys():
+            prototypes[data[f'{i}_in']].append([data[f'{i}_out'], float(data[f'{i}_rib'])])
+        else:
+            prototypes[data[f'{i}_in']] = [[data[f'{i}_out'], float(data[f'{i}_rib'])]]
+    outputs_tops = set(prototypes.keys())
+    for key in prototypes.keys():
+        inp = 0
+        for connection in prototypes[key]:
+            inp += outputs[connection[0]] * connection[1]
+            outputs_tops -= set(connection[0])
+        inputs[key] = round(inp, 2)
+        outputs[key] = round(func(inp, data['Function']), 2)
+    MSE = 0
+    for tops in outputs_tops:
+        if outputs[tops] > 0.5:
+            MSE += pow((outputs[tops]-1), 2)
+        else:
+            MSE += pow(outputs[tops], 2)
+    MSE /= len(outputs_tops)
+    return prototypes, inputs, outputs, MSE
+
+def func(a, f):
+    if f == 'Lineal':
         return a
-    elif func == 2:
-        return 1/(1+pow(e, -a))
-    return((pow(e, 2*a)-1)/(pow(e, 2*a)+1))
-
-print('x; prototypes x; input(x); output(x)')
-for i in inputs.keys():
-    print(i, '-', inputs[i], inputs[i])
-    outputs[i] = inputs[i]
-for i in neurons:
-    cur_input = 0
-    for j in range(len(prototypes[i])):
-        cur_input += outputs[prototypes[i][j]]*weights[i][j]
-    outputs[i] = f(cur_input, func)
-    print(i, ' '.join(map(str, prototypes[i])), round(cur_input, 2), round(outputs[i], 2), sep = "; ")
+    elif f == 'Sigma':
+        return 1 / (1 + pow(e, -a))
+    return (pow(e, 2 * a) - 1) / (pow(e, 2 * a) + 1)
